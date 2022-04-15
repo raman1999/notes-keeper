7b29632197b140e5f1667f15841ed2b3396936ed
raman1999 <45732494+raman1999@users.noreply.github.com>
2022-04-15 02:05:46 -0700

    feat-added-basic-routing

---
+++ src/Pages/index.js
@@ -0,0 +1,7 @@
+export { Archieve } from "./Archieve/Archieve";
+export { Notes } from "./Notes/Notes";
+export { Trash } from "./Trash/Trash";
+export { Home } from "./Home/Home";
+export { Login } from "./Authentication/Login/login";
+export { Signup } from "./Authentication/Signup/signup";
+export { NotFound } from "./NotFound/NotFound";
