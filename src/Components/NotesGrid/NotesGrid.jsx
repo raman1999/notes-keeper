4b1d5dcf394170fad14f88d6e5595a2d39337e87
raman1999 <45732494+raman1999@users.noreply.github.com>
2022-04-21 10:03:44 -0700

    feat: added sidebar

--- src/Components/NotesGrid/NotesGrid.jsx
+++ src/Components/NotesGrid/NotesGrid.jsx
@@ -1,3 +1,11 @@
+import { Outlet } from "react-router-dom";
+import { Sidebar } from "../Sidebar/Sidebar";
+
 export const NotesGrid = () => {
-  return <div>Demo</div>;
+  return (
+    <div className=" grid grid-cols-[18vw_1fr] min-h-[92vh] lg:grid-cols-1 ">
+      <Sidebar />
+      <Outlet />
+    </div>
+  );
 };
