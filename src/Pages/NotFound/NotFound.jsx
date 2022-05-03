611c9f0f0272240316a25dd6561f10286788f671
raman1999 <45732494+raman1999@users.noreply.github.com>
2022-05-03 01:43:07 -0700

    feat: added not found page

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,3 +1,18 @@
+import { Link } from "react-router-dom";
+import { useDocumentTitle } from "../../Hooks/useDocumentTitle";
 export function NotFound() {
-  return <div>Demo</div>;
+  useDocumentTitle("Page Not Found| FitTV");
+  return (
+    <div className="page-not-found flex justify-center items-center   w-full h-[80vh] p-4 dark:text-gray-800">
+      <div className="text-center w-[55%] md:w-[100%] h-[17rem] bg-[#edeecc] flex flex-col  items-center gap-4  p-5 pt-8">
+        <h2 className="text-3xl font-bold text-yellow-800">
+          We cannot find any matches!
+        </h2>
+        <p>The page you are looking for is not available within us.</p>
+        <Link to="/" className="btn p-2">
+          Go to Home &#8594;
+        </Link>
+      </div>
+    </div>
+  );
 }
